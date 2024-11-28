4af6659c1a60c3b837d90a5b70777f310471ec12
param0207 <chaudharyparam0207@gmail.com>
2024-11-28 11:15:21 +0530

    added a feature to add todo

---
+++ script.js
@@ -0,0 +1,19 @@
+// All Elements
+const todoList = document.querySelector("#list");
+const todoInput = document.querySelector("#item-input");
+const form = document.querySelector(".new-item-form");
+
+// event listener
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addItem(todoInput.value);
+  todoInput.value = "";
+});
+
+function addItem(value) {
+  const item = document.createElement("div");
+  item.classList.add("list-item");
+  item.textContent = value;
+
+  todoList.append(item);
+}
